248cb98a7a168ef593b3c56a4023d9af4d56f506
pribylda <davidpribyl09@gmail.com>
2026-05-24 10:14:49 +0200

    feat: add content to obsah-o-nas.php without image path

---
+++ includes/obsah-o-nas.php
@@ -0,0 +1,11 @@
+<div class="obsah">
+    <section class="styl-obsah">
+        <img src="" alt="Fotka chovatelky se psy" class="animate-up"> <!-- přidat cestu k fotce -->
+        <div class="text-obsah">
+            <h2 class="animate-up">O nás</h2>
+            <p class="animate-up">Jsme malá rodinná chovatelská stanice stafordšírských bulteriérů.</p>
+            <p class="animate-up">Pejskové s námi žijí už velmi dlouho. První chlupatí členové rodiny byli Lenny a jeho syn Pedro - labradorští retrívři. V&nbsp;současné době jsou také dva, ale fenečky stafordšírských bulteriérů Alpinka a Bublinka. Všichni jsme propojeni chlupatou láskou. ❤</p>
+            <p class="animate-up">Najdete nás v Plzni.<br>Alpidog Mako</p>
+        </div>
+    </section>
+</div>
