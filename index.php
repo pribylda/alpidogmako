a9051b8cf28b1e80b244e2fd5ad3256b454d7621
pribylda <davidpribyl09@gmail.com>
2026-05-24 09:53:07 +0200

    feat: update header in index.php

--- index.php
+++ index.php
@@ -3,7 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="David Přibyl">
+    <meta name="description" content="Webová stránka rodinné chovné stanice stafordšírských bulteriérů v Plzni.">
+    <meta name="keywords" content="stafordšírský bulteriér, chovná stanice, štěňata, psi, Alpidog Mako, Alpi, Alpidog, Markéta Přibylová, StaffBull, Plzeň">
+    <meta name="robots" content="index, follow">
     <title>Alpidog Mako Chovná stanice</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon"> <!-- přidat cestu k ikoně -->
+    <!-- styly css (přidat) -->
+    <link rel="stylesheet" href="">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TBQQZWDT2G"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-TBQQZWDT2G');
+    </script>
+    <!-- Zobrazení loga, popisu, jména,... -->
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "name": "Alpidog Mako Chovná stanice",
+            "description": "Webová stránka rodinné chovné stanice stafordšírských bulteriérů v Plzni.",
+            "url": "https://alpidogmako.cz/",
+            "logo": "https://alpidogmako.cz/obrazky/logo.png"
+        }
+    </script>
 </head>
 <body>
     
