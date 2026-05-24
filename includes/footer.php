e206c0d71c529ec24417b7819aee06a6657f63af
pribylda <davidpribyl09@gmail.com>
2026-05-24 17:11:13 +0200

    feat: create footer.php with complete code

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="footer-container">
+        <div class="footer-section">
+            <h3>© Chovatelská stanice Alpidog Mako</h3>
+            <p>Web vytvořil <a href="https://www.instagram.com/pribyl.da?igsh=YmkxMmhqbHk1cHUx"><strong>David Přibyl</strong></a></p>
+        </div>
+        <div class="footer-section">
+            <h3>Kontakt</h3>
+            <p><i class="fas fa-map-marker-alt"></i> Plzeň, Česká republika</p>
+            <p><i class="fas fa-phone"></i> <a href="tel:+420777969672">+420 777 969 672</a></p>
+            <p><i class="fas fa-envelope"></i> <a href="mailto:marketapribylova@email.cz">marketapribylova@email.cz</a></p>
+        </div>
+        <div class="footer-section">
+            <h3>Sledujte nás</h3>
+            <p><a href="https://www.instagram.com/alpidogmako" class="social"><i class="fab fa-instagram"></i> Instagram</a></p>
+        </div>
+    </div>
+    <p class="copyright">© Chovatelská stanice Alpidog Mako | Všechna práva vyhrazena</p>
+</footer>
