767fe1418e2c66b370471e6df86ba0379572d6e9
pribylda <davidpribyl09@gmail.com>
2026-05-24 10:24:01 +0200

    feat: create header.php without links and image paths

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<header class="header">
+    <nav class="navbar">
+        <a href="/" class="menulogo"><img src="" alt="logo"></a> #přidat cestu k menu logu
+        <ul class="nav-links">
+            <li><a href="">Úvod</a></li>
+            <li><a href="">Instagram</a></li>
+            <li><a href="">Naši psi</a></li>
+            <li><a href="">Štěňata</a></li>
+            <li><a href="">Odchovy</a></li>
+            <li><a href="">O nás</a></li>
+            <li><a href="" class="btn">Kontakt</a></li>
+        </ul>
+
+        <div class="hamburger" onclick="toggleMenu(this)">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <ul class="mobile-nav">
+            <li><a href="">Úvod</a></li>
+            <li><a href="">Instagram</a></li>
+            <li><a href="">Naši psi</a></li>
+            <li><a href="">Štěňata</a></li>
+            <li><a href="">Odchovy</a></li>
+            <li><a href="">O nás</a></li>
+            <li><a href="">Kontakt</a></li>
+        </ul>
+
+        <div class="overlay" onclick="closeMenu()"></div>
+    </nav>
+</header>
