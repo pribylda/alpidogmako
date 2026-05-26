a405119fcf01d2218876ce0f9c22645006539f60
pribylda <davidpribyl09@gmail.com>
2026-05-26 12:04:15 +0200

    feat: create dog-grid.php with complete code

---
+++ includes/dog-grid.php
@@ -0,0 +1,26 @@
+<section class="dog-grid">
+    <div class="dog-card">
+        <img src="./images/nasi_psi/alpi/Alpi1.jpg" alt="Alpi">
+        <div class="dog-info">
+            <h3>Alpi</h3>
+            <a href="./nasi-psi/alpi/" class="btn">Více info</a>
+        </div>
+    </div>
+    <div class="dog-card">
+        <img src="./images/nasi_psi/bublina/bubli1.jpg" alt="Bublina">
+        <div class="dog-info">
+            <h3>Bublina</h3>
+            <a href="./nasi-psi/bublina/" class="btn">Více info</a>
+        </div>
+    </div>
+    <div class="dog-card">
+        <img src="./images/nasi_psi/pedro/pedro1.jpg" alt="Pedro">
+        <div class="dog-info">
+            <h3>Pedro</h3>
+            <a href="./nasi-psi/pedro/" class="btn">Více info</a>
+        </div>
+    </div>
+</section>
+<div class="more-btn-wrapper">
+    <a href="./nasi-psi/" class="btn btn-big">Všichni naši psi</a>
+</div>
