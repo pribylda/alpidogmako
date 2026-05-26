9ce52293922acdc04a93e7ea248a83c54d3bf373
pribylda <davidpribyl09@gmail.com>
2026-05-26 12:35:49 +0200

    feat: create instagram.php with core layout

---
+++ instagram.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="David Přibyl">
+    <meta name="description" content="Novinky o naší chovné stanici a našich psů.">
+    <meta name="keywords" content="stafordšírský bulteriér, chovná stanice, štěňata, psi, Alpidog Mako, Alpi, Alpidog, Markéta Přibylová, StaffBull, Plzeň">
+    <meta name="robots" content="index, follow">
+    <title>Instagram | Alpidog Mako Chovná stanice</title>
+    <link rel="icon" href="" type="image/x-icon"> <!-- přidat cestu k ikoně -->
+    <!-- styly css (přidat) -->
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    <main>
+        <div id="instagram-widget">
+            <!-- LightWidget WIDGET -->
+            <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+            <iframe src="https://cdn.lightwidget.com/widgets/1306f184d23c5850ae8cdc46ff4c1c2f.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+        </div>
+    </main>
+    <?php include 'includes/footer.php'; ?>
+    <script src="/js/animated-text.js"></script>
+    <script src="/js/burger-menu.js"></script>
+</body>
+</html>
