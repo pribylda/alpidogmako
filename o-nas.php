83008aec34ad69859736d2b314c2e5084b2d779a
pribylda <davidpribyl09@gmail.com>
2026-05-26 12:49:44 +0200

    feat: create o-nas.php with core layout

---
+++ o-nas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="David Přibyl">
+    <meta name="description" content="Webová stránka rodinné chovné stanice stafordšírských bulteriérů v Plzni.">
+    <meta name="keywords" content="stafordšírský bulteriér, chovná stanice, štěňata, psi, Alpidog Mako, Alpi, Alpidog, Markéta Přibylová, StaffBull, Plzeň">
+    <meta name="robots" content="index, follow">
+    <title>O nás | Alpidog Mako Chovná stanice</title>
+    <link rel="icon" href="" type="image/x-icon"> <!-- přidat cestu k ikoně -->
+    <!-- styly css (přidat) -->
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    <main>
+        <?php include 'includes/obsah-o-nas.php'; ?>
+    </main>
+    <?php include 'includes/footer.php'; ?>
+    <script src="/js/animated-text.js"></script>
+    <script src="/js/burger-menu.js"></script>
+</body>
+</html>
