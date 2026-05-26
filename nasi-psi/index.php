0ec733f6cbceee75d9771ba3d0aa153d7de72a61
pribylda <davidpribyl09@gmail.com>
2026-05-26 12:42:40 +0200

    feat: create nasi-psi/index.php with core layout

---
+++ nasi-psi/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="David Přibyl">
+    <meta name="description" content="Psi, kteří nám kdy běhali doma.">
+    <meta name="keywords" content="stafordšírský bulteriér, chovná stanice, štěňata, psi, Alpidog Mako, Alpi, Alpidog, Markéta Přibylová, StaffBull, Plzeň">
+    <meta name="robots" content="index, follow">
+    <title>Naši Psi | Alpidog Mako Chovná stanice</title>
+    <link rel="icon" href="" type="image/x-icon"> <!-- přidat cestu k ikoně -->
+    <!-- styly css (přidat) -->
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    <main>
+        <section class="dog-grid">
+            <div class="dog-card">
+                <img src="" alt="Alpi"> <!-- přidat cestu k obázku -->
+                <div class="dog-info">
+                    <h3>Alpi</h3>
+                    <a href="" class="btn">Více info</a> <!-- přidat cestu na stránku -->
+                </div>
+            </div>
+            <div class="dog-card">
+                <img src="" alt="Bublina"> <!-- přidat cestu k obázku -->
+                <div class="dog-info">
+                    <h3>Bublina</h3>
+                    <a href="" class="btn">Více info</a> <!-- přidat cestu na stránku -->
+                </div>
+            </div>
+            <div class="dog-card">
+                <img src="" alt="Pedro"> <!-- přidat cestu k obázku -->
+                <div class="dog-info">
+                    <h3>Pedro</h3>
+                    <a href="" class="btn">Více info</a> <!-- přidat cestu na stránku -->
+                </div>
+            </div>
+            <div class="dog-card">
+                <img src="" alt="Lenny"> <!-- přidat cestu k obázku -->
+                <div class="dog-info">
+                    <h3>Lenny</h3>
+                    <a href="" class="btn">Více info</a> <!-- přidat cestu na stránku -->
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include 'includes/footer.php'; ?>
+    <script src="/js/animated-text.js"></script>
+    <script src="/js/burger-menu.js"></script>
+</body>
+</html>
