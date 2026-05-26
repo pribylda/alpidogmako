38da6b63cd5417c1c475043e582bddae1305e69c
pribylda <davidpribyl09@gmail.com>
2026-05-26 12:08:44 +0200

    fix: remove incorrect paths and add comments to dog-grid.php

--- includes/dog-grid.php
+++ includes/dog-grid.php
@@ -1,26 +1,26 @@
 <section class="dog-grid">
     <div class="dog-card">
-        <img src="./images/nasi_psi/alpi/Alpi1.jpg" alt="Alpi">
+        <img src="" alt="Alpi"> <!-- přidat cestu k obázku -->
         <div class="dog-info">
             <h3>Alpi</h3>
-            <a href="./nasi-psi/alpi/" class="btn">Více info</a>
+            <a href="" class="btn">Více info</a> <!-- přidat cestu na stránku -->
         </div>
     </div>
     <div class="dog-card">
-        <img src="./images/nasi_psi/bublina/bubli1.jpg" alt="Bublina">
+        <img src="" alt="Bublina"> <!-- přidat cestu k obázku -->
         <div class="dog-info">
             <h3>Bublina</h3>
-            <a href="./nasi-psi/bublina/" class="btn">Více info</a>
+            <a href="" class="btn">Více info</a> <!-- přidat cestu na stránku -->
         </div>
     </div>
     <div class="dog-card">
-        <img src="./images/nasi_psi/pedro/pedro1.jpg" alt="Pedro">
+        <img src="" alt="Pedro"> <!-- přidat cestu k obázku -->
         <div class="dog-info">
             <h3>Pedro</h3>
-            <a href="./nasi-psi/pedro/" class="btn">Více info</a>
+            <a href="" class="btn">Více info</a> <!-- přidat cestu na stránku -->
         </div>
     </div>
 </section>
 <div class="more-btn-wrapper">
-    <a href="./nasi-psi/" class="btn btn-big">Všichni naši psi</a>
+    <a href="" class="btn btn-big">Všichni naši psi</a> <!-- přidat cestu na stránku -->
 </div>
